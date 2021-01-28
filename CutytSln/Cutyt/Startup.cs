841ce9c05a2900a1108c147e282bbbefae5e4ae2
ytodorov <ytodorov@ytodorov.com>
2021-01-29 00:27:45 +0200

    app.UseHttpsRedirection();

--- CutytSln/Cutyt/Startup.cs
+++ CutytSln/Cutyt/Startup.cs
@@ -39,7 +39,7 @@ namespace Cutyt
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            // app.UseHttpsRedirection(); // temp
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
