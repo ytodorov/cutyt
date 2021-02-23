d91f4ecd19dfe3fee4623789fc62acdfb95b364c
ytodorov <ytodorov@ytodorov.com>
2021-02-23 18:07:50 +0200

    Enforce trailing slash - Only for root domain

--- CutytSln/Cutyt/Startup.cs
+++ CutytSln/Cutyt/Startup.cs
@@ -88,8 +88,10 @@ namespace Cutyt
             {
                 //Very problematic. !!!could lead to error: This site can't be reached
                 app.UseRewriter(new RewriteOptions()
-                .AddRedirect("(.*)/$", "$1", (int)HttpStatusCode.MovedPermanently) // Strip trailing slash
+                //.AddRedirect("(.*)/$", "$1", (int)HttpStatusCode.MovedPermanently) // Strip trailing slash
                 //.AddRedirect("(.*[^/])$", "$1/", (int)HttpStatusCode.MovedPermanently) // Enforce trailing slash - Problems with static files - not found with ending slash
+                .AddRedirect("^$", "$1/", (int)HttpStatusCode.MovedPermanently) // // Enforce trailing slash - Only for root domain
+
                 .AddRedirectToWww((int)HttpStatusCode.MovedPermanently) //Very problematic. !!!could lead to error: This site can't be reached
                 .AddRedirectToHttps((int)HttpStatusCode.MovedPermanently)
                 .Add(new RedirectLowerCaseRule())
