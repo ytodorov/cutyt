9ccffee280f1c3541dfaba2542f90063bfa20ab9
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2021-10-25 14:55:50 +0300

    adstera

--- CutytSln/Cutyt.Core/Constants/SiteConstants.cs
+++ CutytSln/Cutyt.Core/Constants/SiteConstants.cs
@@ -24,6 +24,11 @@ namespace Cutyt.Core.Constants
         public static readonly string Description = "Trim, Convert and Download YouTube videos";
 
         /// <summary>
+        /// Trim, Convert and Download YouTube videos
+        /// </summary>
+        public static readonly string DescriptionSeo = "Free download of YouTube videos. Trim and convert YouTube videos to different file formats. Download files in a desired resolution up to 8K. Free and easy to use without any limitations.";
+
+        /// <summary>
         /// https://www.cutyt.com/
         /// </summary>
         public static readonly string SiteName = "https://www.cutyt.com";
