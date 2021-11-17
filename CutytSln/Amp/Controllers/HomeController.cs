5ccfe253b023ec24a27b703c7fe9367939d00f7c
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2021-11-17 19:53:10 +0200

    fix broken download link

--- CutytSln/Amp/Controllers/HomeController.cs
+++ CutytSln/Amp/Controllers/HomeController.cs
@@ -23,6 +23,49 @@ namespace Amp.Controllers
             return View();
         }
 
+        [Route("/watch")]
+        public IActionResult Watch(string v)
+        {
+            ViewResult view = View("Index", $"https://www.youtube.com/watch?v={v}");
+            return view;
+        }
+
+        [Route("url")]
+        public IActionResult Url(string url, string search)
+        {
+            var result = Json(new { url = $"https://stackoverflow.com/questions/50027232/how-to-redirect-a-page-amp/50043339" });
+            return result;
+        }
+
+        [Route("time")]
+        public IActionResult Time()
+        {
+            var result = Json(new { time = $"{DateTime.Now}" });
+            return result;
+        }
+
+        [Route("times")]
+        public IActionResult Times(string name)
+        {
+            
+            var now = new { time = $"{DateTime.Now}" };
+            var yesterday = new { time = $"{DateTime.Now.AddDays(-1)}" };
+
+            List<object> times = new List<object>()
+            { now, yesterday};
+
+            JsonResult result;
+            if (name != null)
+            {
+                result = Json(new { items = times });
+            }
+            else
+            {
+                result = Json(new { items = new List<object>() });
+            }
+            return result;
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
